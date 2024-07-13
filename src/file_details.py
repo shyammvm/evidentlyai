6b665b8d940cc02dd66578cc43ec7f61e003f5e9
Shyam Mohan V M <shyam.vmmohan@gmail.com>
2024-07-13 09:47:39 +0530

    final commit without ui

--- src/file_details.py
+++ src/file_details.py
@@ -1,45 +1,51 @@
 #this class finds and loads the input datasets and metrics associated with it
 
 import os
+import glob
+import yaml
 
 class File_details:
 
     def __init__(self, parameters_path) -> None:
         self.parameters_path = parameters_path
-        self.curr_dir = os.getcwd()
+        self.parameters = self.load_parameters()
 
-    def parse_line(self, line):
-        key, value = line.split('=')
-        key = key.strip()
-        value = value.strip()
-        
-        # Convert value to a list if it contains commas, otherwise leave it as a single value
-        if key == 'numerical' or key == 'categorical':
-            if value == '':
-                value = None  # Set to None if value is empty
-            else:
-                value = [item.strip() for item in value.split(',')]
-        return key, value
+    def load_parameters(self):
+        with open(self.parameters_path, 'r') as file:
+            return yaml.safe_load(file)
+    
+    def get_csv_paths(self):
+        # Define paths to 'current' and 'reference' folders
+        parent_folder = os.path.join('..', 'data', 'dataset', self.parameters['folder'])
+        current_folder = os.path.join(parent_folder, 'current')
+        reference_folder = os.path.join(parent_folder, 'reference')
+
+        # Check if 'current' and 'reference' folders exist
+        if not os.path.isdir(current_folder):
+            raise FileNotFoundError(f"'{current_folder}' folder does not exist.")
+        if not os.path.isdir(reference_folder):
+            raise FileNotFoundError(f"'{reference_folder}' folder does not exist.")
+
+        current_csv_file = glob.glob(os.path.join(current_folder, '*.csv'))
+        reference_csv_file = glob.glob(os.path.join(reference_folder, '*.csv'))
+
+        if not current_csv_file:
+            raise FileNotFoundError(f"No CSV files found in '{current_folder}' folder.")
+        if not reference_csv_file:
+            raise FileNotFoundError(f"No CSV files found in '{reference_folder}' folder.")
+
+        return current_csv_file[0], reference_csv_file[0]
 
     def get_input(self):
-        # Dictionary to store the parameters
-        parameters = {}
-        # Open the file and read the lines into the dictionary
-        with open(self.parameters_path, 'r') as file:
-            for line in file:
-                key, value = self.parse_line(line)
-                parameters[key] = value
 
         # Access the parameters
-        ref_filename = parameters.get('ref_filename')
-        cur_filename = parameters.get('cur_filename')
-        target = parameters.get('target')
-        prediction = parameters.get('prediction')
-        type = parameters.get('model_type')
-        numerical_columns = parameters.get('numerical')
-        categorical_columns = parameters.get('categorical')
-        print(ref_filename)
-        return ref_filename, cur_filename, target, prediction, type, numerical_columns, categorical_columns
-        # dataset_location = os.path.join(self.curr_dir, "data\\dataset")
+        folder = self.parameters['folder']
+        cur_filename, ref_filename = self.get_csv_paths()
+        target = self.parameters['target']
+        prediction = self.parameters['prediction']
+        type = self.parameters['report_type']
+        numerical_columns = self.parameters['numerical_columns']
+        categorical_columns = self.parameters['categorical_columns']
+        return folder, ref_filename, cur_filename, target, prediction, type, numerical_columns, categorical_columns
 
 
--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ import os
 
 def main():
     #get input parameters
-    parameters_path = os.path.join('..', 'data', 'parameters.txt')
+    parameters_path = os.path.join('..', 'data', 'parameters.yaml')
     input_details = File_details(parameters_path)
-    ref_filename, cur_filename, target, prediction, type, numerical_columns, categorical_columns = input_details.get_input()
+    folder, ref_filename, cur_filename, target, prediction, type, numerical_columns, categorical_columns = input_details.get_input()
 
     #get report
-    report_obj = Reports(cur_file=cur_filename, ref_file=ref_filename, target=target, prediction=prediction, numerical_columns=numerical_columns, categorical_columns=categorical_columns, type=type)
+    report_obj = Reports(folder=folder, cur_file=cur_filename, ref_file=ref_filename, target=target, prediction=prediction, numerical_columns=numerical_columns, categorical_columns=categorical_columns, type=type)
     report = report_obj.get_report()
+    report_obj.save_report(report)
     print(report)
 
 
